4e8d4b5b6c312752a096d5f6dac55268b722a635
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2023-08-31 01:24:07 +0900

    Update pages

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,5 +1,6 @@
 import Navbar from '@/components/navbar/navbar'
 import { TailwindIndicator } from '@/components/tailwind-indicator'
+import { ScrollArea } from '@radix-ui/react-scroll-area';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 /* import Home from './home' */
@@ -12,10 +13,12 @@ import { BrowserRouter as Router } from 'react-router-dom';
      return (
     <>
       <TailwindIndicator />
+        <ScrollArea className="h-screen rounded-md border">
         <Router>
           <Navbar />
           {children}
         </Router>
+        </ScrollArea>
     </>
   )
 }
--- src/page.tsx
+++ src/page.tsx
@@ -1,13 +1,19 @@
 import Dashboard from './dashboard'
 import { Routes, Route } from 'react-router-dom'
 import Tasks from './tasks'
+import Contacts from './contacts'
+import Projects from './projects'
+import Desk from './desk'
 
 export default function Home() {
   return (
     <main className='xl:mx-36 2xl:mx-56'>
      <Routes>
             <Route path="/" element={<Dashboard />} />
+            <Route path="projects" element={<Projects />} />
             <Route path="tasks" element={<Tasks />} />
+            <Route path="desk" element={<Desk />} />
+            <Route path="contacts" element={<Contacts />} />
           </Routes>
     </main>
   )
