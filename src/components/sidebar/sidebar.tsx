4f34f133a8df99a631bf4f0c80328963c484d687
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2023-09-01 09:46:19 +0900

    Fix overflow bug

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,6 +1,5 @@
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
-import { ScrollArea } from "@/components/ui/scroll-area"
 import { items } from "./items"
 import { Link } from "react-router-dom"
 
